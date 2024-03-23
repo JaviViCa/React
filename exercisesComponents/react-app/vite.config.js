fa76e59f8adc2d5ccae87329d72c9cec75a20cd3
JaviViCa <vicaxa2010@gmail.com>
2024-03-23 18:26:38 +0100

    Exercise 4

--- exercisesComponents/react-app/vite.config.js
+++ exercisesComponents/react-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Exercise Components 4
+  // server: {
+  //   port: 3000,
+  // },
 })
