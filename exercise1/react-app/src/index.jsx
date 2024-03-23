56e7b83fb6c452e3f881e7dfe86f2947ac0dad8f
JaviViCa <vicaxa2010@gmail.com>
2024-03-23 17:13:15 +0100

    exercise 1

---
+++ exercise1/react-app/src/index.jsx
@@ -0,0 +1,11 @@
+import {createRoot} from "react-dom/client";
+
+// Exercise Componnent 1
+// function Hello() {
+//     return <h2>Hello, World!</h2>;
+// }
+// function App() {
+//     return <div><Hello /></div>
+// }
+// const root = createRoot(document.getElementById("root"));
+// root.render(<App />);
